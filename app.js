2566c1f50d94c8fdcb05b50ae7e436c72ce4087a
Akhand Pratap Singh <ankitsingha4p7s@gmail.com>
2025-03-24 13:50:22 +0530

    getting started

---
+++ app.js
@@ -0,0 +1,9 @@
+const amount=12
+
+if(amount<10){
+    console.log(amount+' is a small number') 
+}
+else{
+    console.log(amount+' is a large number')
+}
+console.log('hey its my first node app!!!')
